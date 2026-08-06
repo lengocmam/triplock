45b07db3cfa4511f5d9596f5272fa293eed88e1e
lengocmam <email_cua_ban@gmail.com>
2026-08-06 14:07:18 +0700

    update process.env in backend/src/main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: [
-      process.env.FRONTEND_URL || 'http://localhost:5173',
+      process.env.FRONTEND_URL,
       'http://localhost:5173',
     ],
     credentials: true,
